8a423044f5b6c236dc5a1c23c0d6777521f33959
felipeam10 <felipeam10@hotmail.com>
2021-07-06 00:55:56 -0300

    commit v2.1 classes iniciais do projeto

--- src/main/java/br/com/forum/springbootv1/SpringbootV1Application.java
+++ src/main/java/br/com/forum/springbootv1/SpringbootV1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootV1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootV1Application.class, args);
 	}
 
